26e8fd31d131897b8ff72a430037d9fe98f3e8e2
Dmitry <chaedmitry@gmail.com>
2019-12-01 00:50:54 +0300

    One sound play at a time, but allTracks[i] undef.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function playPause(id) {
+document.addEventListener("DOMContentLoaded", function() {
+    pauseAllTracks(document.body);
+  });
+  
+  function pauseAllTracks (grid) {
+    grid.addEventListener("play", function(event) {
+    let allTracks = document.getElementsByTagName('audio');
+        for (let i = 0; i <= allTracks.length; i++) { 
+            if (allTracks[i] !== event.target) {
+                allTracks[i].pause();
+                allTracks[i].currentTime = 0;
+            }
+        }
+    }, true);
+}
+
+  function playPause(id) {
     id.paused ? id.play() : id.pause();
 }
