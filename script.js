8c94a6d2b655d61c2964349b6a8b78495f307135
Dmitry <chaedmitry@gmail.com>
2020-02-14 22:08:40 +0000

    new icons, trying to change icon onclick

--- script.js
+++ script.js
@@ -1,32 +1,69 @@
 document.addEventListener("DOMContentLoaded", 
     function() {
-        pauseAllTracks(document.body);
+        stopAnotherTrack(document.body);
+        pauseToPlayOnEnd(document.body);
+        pauseToPlayOnPause(document.body);
+        playToPauseOnPlay(document.body);
     }
 );
 
-function pauseAllTracks (grid) {
+var tracks = document.getElementsByTagName('audio');
+var icons = document.getElementsByTagName('use');
+var iconPlay = document.getElementById('icon-play');
+var iconPause = document.getElementById('icon-pause');
+
+function stopAnotherTrack (grid) { /* Stop all other tracks when click on play */
     grid.addEventListener("play",
         function() {
-        let allTracks = document.getElementsByTagName('audio');
-            for (let i = 0; i <= allTracks.length; i++) {
-            let track = allTracks[i];
-                if (track !== event.target) {
-                    track.pause();
-                    track.currentTime = 0;
+            for (let i = 0; i <= tracks.length; i++) {
+                if (tracks[i] !== event.target) {
+                    tracks[i].pause();
+                    tracks[i].currentTime = 0;
                 }
             }   
         },
     true);
 }
 
-function playPause(trackID, iconID) {
-    if (trackID.paused === true) {
+function pauseToPlayOnEnd (tracks) {  /* Reset all icons to play when some audio ended */
+    tracks.addEventListener("ended",
+        function () {
+            for (let i = 0; i <= icons.length; i++) {
+                icons[i].setAttribute('href',"#icon-play");
+            }
+    }, true);
+}
+
+function pauseToPlayOnPause (icons) {
+    icons.addEventListener("pause",
+        function () {
+            for (let i = 0; i <= icons.length; i++) {
+                if (icons[i].getAttribute('href') = "#icon-pause") {
+                    icons[i].setAttribute('href',"#icon-play");
+                }
+            }
+    }, true);
+}
+
+function playToPauseOnPlay (icons) {
+    icons.addEventListener("play",
+        function () {
+            for (let i = 0; i <= icons.length; i++) {
+                if (icons[i].getAttribute('href') = "#icon-play") {
+                    icons[i].setAttribute('href',"#icon-pause");
+                }
+            }
+    }, true);
+}
+
+function playPause(trackID, iconID) { 
+    if (trackID.paused) {
         trackID.play();
-        iconID.setAttribute('href',"#icon-pause");
-    } 
+        iconID.setAttribute('href', '#icon-pause');
+    }
     else {
         trackID.pause();
-        iconID.setAttribute('href',"#icon-play");
+        iconID.setAttribute('href', '#icon-play');
     }
 }
 
