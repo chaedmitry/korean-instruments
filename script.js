04a97515149da99fdd990955a5fb22a6cdcbed1f
Dmitry <chaedmitry@gmail.com>
2020-03-15 16:42:55 +0000

    playPause conditional behavior (local paths)

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
 document.addEventListener("DOMContentLoaded", 
     function() {
-        pauseToPlayOnEnd(document.body);
-        pauseToPlayOnPause(document.body);
-        playToPauseOnPlay(document.body);
+//        pauseToPlayOnEnd(document.body);
+//        pauseToPlayOnPause(document.body);
+//        playToPauseOnPlay(document.body);
     }
 );
 
 //Audio
 let track = document.getElementById('track');
+let tracks = [
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackGayageum.mp3', //0
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackGeomungo.mp3', //1
+    '...', //2
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackHaegeum.mp3', //3
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackJanggo.mp3', //4
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackJing.mp3', //5
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackBuk.mp3', //6
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackKkwaenggwari.mp3', //7
+    '...', //8
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackDaegeum.mp3', //9
+    '...', //10
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackDanso.mp3', //11
+    'file:///Users/dmitrychae/korean-instruments/sounds/trackHyangPiri.mp3' //12
+] 
 
 //Icons
 let icons = document.getElementsByTagName('use');
 let iconPlay = document.getElementById('icon-play');
 let iconPause = document.getElementById('icon-pause');
 
-//Tracks paths
-let trackGayageum = 'sounds/trackGayageum.mp3';
-let trackGeomungo = 'sounds/trackGeomungo.mp3';
-let trackBuk = 'sounds/trackBuk.mp3';
-let trackDaegeum = 'sounds/trackDaegeum.mp3';
-let trackDanso = 'sounds/trackDanso.mp3';
-let trackHaegeum = 'sounds/trackHaegeum.mp3';
-let trackHyangPiri = 'sounds/trackHyangPiri.mp3';
-let trackJanggo = 'sounds/trackJanggo.mp3';
-let trackJing = 'sounds/trackJing.mp3';
-let trackKkwaenggwari = 'sounds/trackKkwaenggwari.mp3';
+
+function playPause(i) {
+    if (track.paused) {
+        track.src = tracks[i];
+        track.play();
+    }
+    else if (tracks.indexOf(track.src) === i) {
+        track.pause();
+    }
+    else {
+        track.src = tracks[i];
+        track.play();
+    }
+}
 
 //Reset all icons to play when some audio ended
+/*
 function pauseToPlayOnEnd (track) {  
     track.addEventListener("ended",
         function () {
@@ -57,9 +76,4 @@ function playToPauseOnPlay (icons) {
             }
     }, true);
 }
-
-function playPause(trackID) { 
-        track.src = trackID;
-        track.play();
-}
-
+*/
