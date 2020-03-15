37579ff1564096cc101c70c6ae763b0d5cf3fd84
Dmitry <chaedmitry@gmail.com>
2020-03-15 16:56:28 +0000

    prepend github paths

--- script.js
+++ script.js
@@ -9,19 +9,19 @@ document.addEventListener("DOMContentLoaded",
 //Audio
 let track = document.getElementById('track');
 let tracks = [
-    'sounds/trackGayageum.mp3', //0
-    'sounds/trackGeomungo.mp3', //1
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackGayageum.mp3', //0
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackGeomungo.mp3', //1
     '...', //2
-    'sounds/trackHaegeum.mp3', //3
-    'sounds/trackJanggo.mp3', //4
-    'sounds/trackJing.mp3', //5
-    'sounds/trackBuk.mp3', //6
-    'sounds/trackKkwaenggwari.mp3', //7
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackHaegeum.mp3', //3
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackJanggo.mp3', //4
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackJing.mp3', //5
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackBuk.mp3', //6
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackKkwaenggwari.mp3', //7
     '...', //8
-    'sounds/trackDaegeum.mp3', //9
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackDaegeum.mp3', //9
     '...', //10
-    'sounds/trackDanso.mp3', //11
-    'sounds/trackHyangPiri.mp3' //12
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackDanso.mp3', //11
+    'https://chaedmitry.github.io/korean-instruments/sounds/trackHyangPiri.mp3' //12
 ] 
 
 //Icons
