58e0a19acb2bcef579fc85794a11cd3cd63a312e
Dmitry <chaedmitry@gmail.com>
2020-04-02 22:47:01 +0100

    basic styles quiz app

---
+++ quiz.js
@@ -0,0 +1,9 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
